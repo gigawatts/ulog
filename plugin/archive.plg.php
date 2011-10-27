f8c579d10baa143afb7eff44007fdea3bf23a8dc
root <root@localhost.localdomain>
2011-10-27 01:19:38 -0700

    merge common js

--- plugin/archive.plg.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function archive_head()
-{
-  return '<script src="js/archive.js"></script>';
-}
-
-?>
---
+++ plugin/common.plg.php
@@ -0,0 +1,11 @@
+<?php
+
+function common_head()
+{
+	return '<script src="js/candy.js"></script>
+	<script src="js/bbcode.js"></script>
+	<script src="js/highlight.js"></script>
+	<script src="js/archive.js"></script>';
+}
+
+?>
