80f545c18daca7c492582524ce4da6b7882f8998
Your Name <march@localhost.localdomain>
2011-10-27 03:39:10 -0700

    remove merge plugin page into view

--- view.php
+++ view.php
@@ -90,6 +90,13 @@ else if(isGET('archive') && strlen($_GET['archive']) === 7)
 	}
 	$out['content'] .= '</ul>';
 }
+else if(isGET('plugin') && function_exists($_GET['plugin']. '_page'))
+{
+	$misc = $_GET['plugin']. '_page';
+	$out['subtitle'] = $_GET['plugin'];
+	$out['content'] .= '<h1>' .$out['subtitle']. '</h1>'.
+	$misc();
+}
 else
 {
 	exit;
