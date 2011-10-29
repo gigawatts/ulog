b8b1ed90a818f648612762f030cea5e081c607ba
root <root@localhost.localdomain>
2011-10-29 15:07:32 -0700

    update js

--- js/archive.js
+++ js/archive.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-	$("div#sidebar ul:last-child li").hide();
-	$("div#sidebar ul:last-child").append('<li id="archiveHover">[...]</li>');
-	$("li#archiveHover").hover(
+	$('div#sidebar ul:last-child li').hide();
+	$('div#sidebar ul:last-child').append('<li id="archiveHover">[...]</li>');
+	$('li#archiveHover').hover(
 		function() {
-		        $("div#sidebar ul:last-child li").show("fast");
+		        $('div#sidebar ul:last-child li').show('fast');
 		}
 	);
 });
