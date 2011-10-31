17dacd8102ce4e4bd591af6363bfae7df9dd591d
taylorchu <tailinchu@gmail.com>
2011-10-31 09:31:45 -0700

    Update js/archive.js

--- js/archive.js
+++ js/archive.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-	$('#sidebar ul:last-child li').hide();
-	$('<li>[...]</li>').appendTo('#sidebar ul:last-child')
-	.hover(
+	$('#sidebar ul:last-child li a').hide();
+	$('#sidebar ul:last-child li').click(
 		function() {
-		        $('#sidebar ul:last-child li').show('fast');
-		        $(this).remove();
+		        $(this).children('a').fadeToggle();
 		}
 	);
 });
