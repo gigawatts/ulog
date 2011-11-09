26d9e07af2185fc6113f2e591b3d7971170385f1
Your Name <march@localhost.localdomain>
2011-11-09 00:01:07 -0800

    js update

--- plugin/common.plg.php
+++ plugin/common.plg.php
@@ -4,8 +4,8 @@ function common_head()
 {
 	return '<script src="js/candy.js"></script>
 	<script src="js/bbcode.js"></script>
-	<script src="js/highlight.js"></script>
 	<script src="js/archive.js"></script>
+	<script src="js/loadreply.js"></script>
 	<script src="js/loadform.js"></script>';
 }
 
