f3be8fbaf17138b1f50e695f3a02e9886020d906
Your Name <march@localhost.localdomain>
2011-11-21 10:29:32 -0800

    revert

---
+++ lang/en.lng.php
@@ -0,0 +1,44 @@
+<?php
+
+$lang['post'] = 'Post';
+$lang['reply'] = 'Reply';
+$lang['plugin'] = 'Plugin';
+$lang['more'] = 'More';
+$lang['config'] = 'Config ';
+$lang['logout'] = 'Logout';
+$lang['login'] = 'Login';
+$lang['redirect'] = 'Go back to';
+$lang['add'] = 'Add ';
+$lang['edit'] = 'Edit ';
+$lang['delete'] = 'Delete ';
+$lang['title'] = 'Title';
+$lang['content'] = 'Content';
+$lang['name'] = 'Name';
+$lang['view'] = 'View';
+$lang['search'] = 'Search';
+$lang['link'] = 'Link';
+$lang['category'] = 'Category';
+$lang['archive'] = 'Archive';
+$lang['url'] = 'URL';
+$lang['password'] = 'Password';
+$lang['poweredBy'] = 'Powered by';
+$lang['feed'] = 'Feed';
+$lang['theme'] = 'Theme';
+$lang['lang'] = 'Language';
+$lang['none'] = 'No entry so far';
+$lang['confirm'] = 'ok';
+$lang['uncategorized'] = 'Uncategorized';
+$lang['yes'] = 'Yes';
+$lang['no'] = 'No';
+$lang['locked'] = 'Locked';
+$lang['day'] = 'day';
+$lang['hour'] = 'hour';
+$lang['minute'] = 'minute';
+$lang['second'] = 'second';
+$lang['plural'] = 's';
+$lang['ago'] = 'ago';
+$lang['errorLength'] = ' is too short or too long';
+$lang['errorBot'] = 'Incorrect CAPTCHA';
+$lang['replied'] = 'replied to';
+
+?>
