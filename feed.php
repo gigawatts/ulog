5843db5c305d4c6340fbac251fb748da2ef0f774
Your Name <march@localhost.localdomain>
2011-12-15 03:06:02 -0800

    improve performance (beta)

--- feed.php
+++ feed.php
@@ -11,7 +11,7 @@ if(isGET('post'))
 {
 	$out['subtitle'] = $lang['post'];
 	$out['type'] = 'post';
-	$posts = part('rsort', listEntry('post'), 4);
+	$posts = _max(listEntry('post'), 4);
 	if($posts)
 	{
 		foreach($posts as $post)
@@ -32,7 +32,7 @@ else if(isGET('reply'))
 {
 	$out['subtitle'] = $lang['reply'];
 	$out['type'] = 'reply';
-	$replies = part('rsort', listEntry('reply'), 4);
+	$replies = _max(listEntry('reply'), 4);
 	if($replies)
 	{
 		foreach($replies as $reply)
