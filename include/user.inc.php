07cc83cebeae4c910d34ee007e89641d815451c2
taylorchu <tailinchu@gmail.com>
2012-03-23 12:56:21 -0700

    Update include/user.inc.php

--- include/user.inc.php
+++ include/user.inc.php
@@ -33,11 +33,11 @@ function quote($reply)
 	{
 		$replyEntry = readEntry('reply', $reply);
 		$postEntry = readEntry('post', $replyEntry['post']);
-		return '<a class="label label-info" href="view.php/post/' .$replyEntry['post']. '/p/' .onPage($reply, $postEntry['reply']). '#' .$reply. '">' .$replyEntry['trip']. '</a>';
+		return '<p><a class="label label-info" href="view.php/post/' .$replyEntry['post']. '/p/' .onPage($reply, $postEntry['reply']). '#' .$reply. '">' .$replyEntry['trip']. '</a></p>';
 	}
 	else
 	{
-		return '<a class="label label-info">[?]</a>';
+		return '<p><a class="label label-info">[?]</a></p>';
 	}
 }
 
