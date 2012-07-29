9fede7bd8265b01d7cee68993448b0d99aa19bef
Ken Johnson <gigawatts51@gmail.com>
2012-07-29 03:42:12 -0500

    removing now unused auth.php file and removed debug 'login and logout' links from extra.php

--- extra.php
+++ extra.php
@@ -1,8 +1,6 @@
-<html><title>ULog settings links</title>
+<html><title>Configuration</title>
 <ul><h3>Extra Configuration</h3>
 <li><a href="config.php/main">Config</a>
 <li><a href="config.php/plugin">Plugin</a>
-<li><a href="auth.php/login">Login</a>
-<li><a href="auth.php/logout">Logout</a>
 </ul>
 </html>
