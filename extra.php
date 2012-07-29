ac57eea96d9ebd648684c945b1e4c7b6fd7d66bb
root <root@Shuttle.local>
2012-07-29 02:58:06 -0500

    adding newly referenced files

---
+++ extra.php
@@ -0,0 +1,8 @@
+<html><title>ULog settings links</title>
+<ul><h3>Extra Configuration</h3>
+<li><a href="config.php/main">Config</a>
+<li><a href="config.php/plugin">Plugin</a>
+<li><a href="auth.php/login">Login</a>
+<li><a href="auth.php/logout">Logout</a>
+</ul>
+</html>
